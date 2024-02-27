204d7a89b3ecd248545c1ee63fcdf14dcca5879b
DocentSzachista <idontknow5325@gmail.com>
2024-02-27 14:22:47 +0100

    add dir creation

--- packing_util.py
+++ packing_util.py
@@ -2,8 +2,9 @@ import json
 import shutil
 from testing_layer.configuration import Config
 from testing_layer.workflows.enums import SupportedModels
-
+import os
 if __name__ == "__main__":
+    os.makedirs("zipfiles", exist_ok=True)
     with open("./config-imagenet.json", "r") as file:
         obj = json.load(file)
         models = [SupportedModels(model) for model in obj.get("models")]
@@ -12,4 +13,4 @@ if __name__ == "__main__":
             obj['model'] = tested_model.value
             print(f"Make archive for model {tested_model.value}")
             config = Config(obj)
-            shutil.make_archive( f"{config.model}-{config.tag}", "zip", f"counted_outputs/{config.model_base_dir}")
+            shutil.make_archive( f"./zipfiles/{config.model}-{config.tag}", "zip", f"counted_outputs/{config.model_base_dir}")
